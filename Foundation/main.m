61346095f929aff16593f1019ab0294903dc0f4e
wf90163 <wf90163@alibaba-inc.com>
2017-01-13 21:59:33 +0800

    Initial Commit

---
+++ Foundation/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Foundation
+//
+//  Created by wangfei on 17/1/13.
+//  Copyright © 2017年 wangfei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
